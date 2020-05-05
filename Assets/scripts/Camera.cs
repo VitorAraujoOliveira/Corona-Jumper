ddff0773640c3cbc2c86953f066f02d1678eef39
VitorAraujoOliveira <vitor.araujo934@hotmail.com>
2020-05-05 16:38:53 -0300

    v0.0.2

--- Assets/scripts/Camera.cs
+++ Assets/scripts/Camera.cs
@@ -8,10 +8,7 @@ public class Camera : MonoBehaviour
     public GameObject corona;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
